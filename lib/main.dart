55e3e7134b461e094faad7afd5c3b0b0979b6a59
rakeshdtechex <rakeshdtechex456@gmail.com>
2023-11-07 12:28:49 +0530

    added functionality

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
+  runApp(const MyApp());
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  await service.configure(
+      iosConfiguration: IosConfiguration(),
+      androidConfiguration: AndroidConfiguration(
+          onStart: onStart, isForegroundMode: true, autoStart: false));
+}
+
+@pragma('va:entry-point')
+void onStart(ServiceInstance service) async {
+
+  StreamSubscription? streamSubscription;
+  if (service is AndroidServiceInstance) {
+    service.setForegroundNotificationInfo(title: 'order thali on background or kill', content: 'add conentet here');
+    service.on('setAsForeground').listen((event) {
+      service.setAsBackgroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+
+    service.on('stopService').listen((event) {
+      service.stopSelf();
+    });
+
+    Timer.periodic(const Duration(seconds: 2), (timer) async {
+      if (await service.isForegroundService()) {
+
+        streamSubscription = Connectivity().onConnectivityChanged
+            .listen((ConnectivityResult result) {
+          if (result == ConnectivityResult.wifi) {
+
+          }else{
+            service.invoke('stopService');
+          }
+        });
+        print('api call if wifi is availalbe');
+      }
+
+    });
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String text = "Stop Service";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Service App'),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  child: const Text("Foreground Mode"),
+                  onPressed: () {
+                    FlutterBackgroundService().invoke("setAsForeground");
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text("Background Mode"),
+                  onPressed: () {
+                    FlutterBackgroundService().invoke("setAsBackground");
+                  },
+                ),
+                ElevatedButton(
+                  child: Text(text),
+                  onPressed: () async {
+                    final service = FlutterBackgroundService();
+                    var isRunning = await service.isRunning();
+                    if (isRunning) {
+                      service.invoke("stopService");
+                    } else {
+                      service.startService();
+                    }
+
+                    if (!isRunning) {
+                      text = 'Stop Service';
+                    } else {
+                      text = 'Start Service';
+                    }
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
